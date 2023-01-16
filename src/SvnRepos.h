b12db1af7ada13dbe9cfdaf77752874f66bb5089
Sylvain Trinquet <sylvain.trinquet@gmail.com>
2023-01-16 15:56:58 +0100

    create the SvnRepos class and its pimpl

---
+++ src/SvnRepos.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <filesystem>
+#include <memory>
+
+class SvnReposImpl;
+class SvnRepos final
+{
+  // delete copy/assignement operators
+  SvnRepos(const SvnRepos&) = delete;
+  SvnRepos& operator=(const SvnRepos&) = delete;
+  SvnRepos(SvnRepos&&) = delete;
+  SvnRepos& operator=(SvnRepos&&) = delete;
+
+public:
+  // constructor/destructor
+  SvnRepos();
+  ~SvnRepos();
+
+  // start all the threads to update svn repositories
+  void update(const std::vector<std::filesystem::path>& repos);
+
+private:
+  // pointer to internal implementation
+  std::unique_ptr<SvnReposImpl> m_pimpl;
+};
--- src/svn-update.cpp
+++ src/svn-update.cpp
@@ -1,13 +1,9 @@
 #include <string>
-#include <vector>
-#include <queue>
-#include <deque>
-#include <map>
-#include <regex>
 #include <filesystem>
 #include <memory>
-#include <thread>
-#include <mutex>
+#include <vector>
+#include <sstream>
+#include <regex>
 #include <stdbool.h>
 #include <fmt/core.h>
 #include <fmt/color.h>
@@ -16,18 +12,15 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <winpp/console.hpp>
 #include <winpp/parser.hpp>
-#include <winpp/utf8.hpp>
-#include <winpp/progress-bar.hpp>
 #include <winpp/files.hpp>
-#include <winpp/win.hpp>
-#include <fort.hpp>
+#include "SvnRepos.h"
 
 /*============================================
 | Declaration
 ==============================================*/
 // program version
 const std::string PROGRAM_NAME = "svn-update";
-const std::string PROGRAM_VERSION = "1.1";
+const std::string PROGRAM_VERSION = "1.2";
 
 /*============================================
 | Function definitions
@@ -107,139 +100,6 @@ std::vector<std::filesystem::path> extract_dirs(const std::string& root,
   }
   return dirs;
 }
-
-class SvnRepos final
-{
-public:
-  // constructor
-  SvnRepos() :
-    m_repos(),
-    m_results(),
-    m_progress_bar(),
-    m_nb_repos(),
-    m_running(false),
-    m_threads(),
-    m_mutex()
-  {
-  }
-
-  // destructor
-  ~SvnRepos()
-  {
-    // stop threads
-    m_running = false;
-    for (auto& t : m_threads)
-      if (t.joinable())
-        t.join();
-  }
-
-  // start all the threads to update svn repositories
-  void update(const std::vector<std::filesystem::path>& repos)
-  {
-    // initialize update variables
-    m_repos = std::queue(std::deque(repos.begin(), repos.end()));
-    m_nb_repos = m_repos.size();
-    m_results.clear();
-
-    {
-      // create progress-bar
-      m_progress_bar = std::make_unique<console::progress_bar>("update svn repositories: ", m_nb_repos);
-      
-      // start threads
-      spdlog::debug(fmt::format(fmt::emphasis::bold, "launch the svn update commands on repositories:\n"));
-      m_running = true;
-      const std::size_t max_cpu = static_cast<std::size_t>(std::thread::hardware_concurrency());
-      m_threads = std::vector<std::thread>(std::min(m_nb_repos, max_cpu));
-      for (auto& t : m_threads)
-        t = std::thread(&SvnRepos::run, this);
-
-      // wait for threads completion
-      for (auto& t : m_threads)
-        if (t.joinable())
-          t.join();
-      
-      // delete progress-bar
-      m_progress_bar.reset();
-    }
-
-    // log updated repositories as table
-    log();
-  }
-
-private:
-  // run the update process - thread
-  void run()
-  {
-    while (m_running)
-    {
-      // retrieve one repos - protected by mutex
-      std::filesystem::path repo;
-      {
-        std::lock_guard<std::mutex> lck(m_mutex);
-        if (m_repos.empty())
-          return;
-        repo = m_repos.front().parent_path();
-        m_repos.pop();
-      }
-
-      // execute the update process
-      std::string logs;
-      std::regex update_ok(R"(^A |^D |^U |^C |^G |E^ |R^ )");
-      int exit_code = win::execute("svn.exe update --accept theirs-full", logs, repo);
-
-      // store result and update progress-bar - protected by mutex
-      {
-        std::lock_guard<std::mutex> lck(m_mutex);
-        if (exit_code != 0)
-          m_results[repo] = false;
-        else if (std::regex_search(logs, update_ok))
-          m_results[repo] = true;
-        m_progress_bar->tick();
-      }
-    }
-  }
-
-  void log()
-  {
-    if (!m_results.empty())
-    {
-       // create table stylesheet
-      fort::utf8_table table;
-      table.set_border_style(FT_NICE_STYLE);
-      table.column(0).set_cell_text_align(fort::text_align::left);
-      table.column(0).set_cell_content_text_style(fort::text_style::bold);
-      table.column(1).set_cell_text_align(fort::text_align::center);
-      table.column(1).set_cell_content_text_style(fort::text_style::bold);
-
-      // create header
-      table << fort::header << "PROJECTS" << "UPDATED" << fort::endr;
-
-      // add rows
-      std::size_t idx = 1;
-      for (const auto& r : m_results)
-      {
-        table << utf8::to_utf8(r.first.string()) << (r.second ? "OK" : "KO") << fort::endr;
-        table[idx++][1].set_cell_content_fg_color(r.second ? fort::color::green : fort::color::red);
-      }
-      spdlog::info("{}\n\n", table.to_string());
-    }
-    spdlog::info("total repositories updated: [{}/{}]\n", m_results.size(), m_nb_repos);
-  }
-
-private:
-  // svn repos informations
-  std::queue<std::filesystem::path> m_repos;
-  std::map<std::filesystem::path, bool> m_results;
-
-  // progress-bar
-  std::unique_ptr<console::progress_bar> m_progress_bar;
-  std::size_t m_nb_repos;
-
-  // handling threads
-  std::atomic_bool m_running;
-  std::vector<std::thread> m_threads;
-  std::mutex m_mutex;
-};
 
 int main(int argc, char** argv)
 {
